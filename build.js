2655812959e834798c3217444d21a5aa5ca423f6
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-11-01 22:11:25 +0100

    build

---
+++ build.js
@@ -0,0 +1,15 @@
+import { readFile, rm, writeFile } from "fs/promises";
+
+const wasm = await readFile("./wasm/pkg/zepar_bg.wasm");
+
+await writeFile(
+  `./wasm/pkg/zepar.wasm.js`,
+  `export const wasm = "${wasm.toString("base64")}";`
+);
+
+await writeFile(
+  `./wasm/pkg/zepar.wasm.d.ts`,
+  `export const wasm: string;`
+);
+
+await rm(`./wasm/pkg/.gitignore`);
