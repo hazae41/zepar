e5c8b627abe0f886b8824c580fc0364ac6097ac1
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-11-05 15:45:52 +0100

    better errors

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,36 +1,3 @@
 #![no_std]
 
-extern crate alloc;
-
-use alloc::boxed::Box;
-
-use wasm_bindgen::prelude::*;
-
-type Aes128Ctr128BE = ctr::Ctr128BE<aes::Aes128>;
-
-#[wasm_bindgen]
-pub struct Aes128Ctr128BEKey {
-    inner: Box<Aes128Ctr128BE>,
-}
-
-#[wasm_bindgen]
-impl Aes128Ctr128BEKey {
-    #[wasm_bindgen(constructor)]
-    pub fn new(key: &[u8], iv: &[u8]) -> Result<Aes128Ctr128BEKey, JsValue> {
-        use ctr::cipher::KeyIvInit;
-
-        let key16: [u8; 16] = key.try_into().map_err(|_| JsValue::from("Error"))?;
-        let iv16: [u8; 16] = iv.try_into().map_err(|_| JsValue::from("Error"))?;
-        let cipher = Aes128Ctr128BE::new(&key16.into(), &iv16.into());
-        let inner = Box::new(cipher);
-
-        Ok(Self { inner })
-    }
-
-    #[wasm_bindgen]
-    pub fn apply_keystream(&mut self, buf: &mut [u8]) -> () {
-        use ctr::cipher::StreamCipher;
-
-        self.inner.apply_keystream(buf);
-    }
-}
+mod aes128ctr128be;
